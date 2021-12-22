073e519ee7c6cc2a151fd85b7425b34b245d0cad
yanmingpc123@163.com <yanmingpc123@163.com>
2021-12-22 22:02:02 +0800

    添加404

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,6 +4,7 @@ import Home from '@/views/Home.vue'
 import Money from '@/views/Money.vue'
 import Labels from '@/views/Labels.vue'
 import Statistics from '@/views/Statistics.vue'
+import NotFound from '@/views/NotFound.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,9 @@ const routes = [
   }, {
     path: '/statistics',
     component: Statistics
+  },{
+    path: '*',
+    component: NotFound
   }
 ]
 
