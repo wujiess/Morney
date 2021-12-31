4be8a6f2ab3b16755b4c8648337ad0513238c60c
wujiess <yanmingpc123@163.com>
2021-12-31 17:47:48 +0800

    模块化：把 Money.vue 分成四个组件

---
+++ src/components/Money/Notes.vue
@@ -0,0 +1,33 @@
+<template>
+  <label class="notes">
+    <span class="name">备注</span>
+    <input type="text" placeholder="在这里输入备注"
+  /></label>
+</template>
+
+<script>
+export default {
+  name: "Notes",
+};
+</script>
+
+<style lang="scss" scoped>
+.notes {
+  background-color: #f5f5f5;
+  display: block;
+  font-size: 14px;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+  .name {
+    padding-right: 16px;
+  }
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+</style>
---
+++ src/components/Money/Types.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul class="types">
+    <li class="selected">支出</li>
+    <li>收入</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Types",
+};
+</script>
+
+<style lang="scss" scoped>
+.types {
+  background-color: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      bottom: 0;
+      width: 100%;
+      height: 4px;
+      background-color: #333;
+    }
+  }
+}
+</style>
