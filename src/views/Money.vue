fa4726dd8c37db609db10b3fffab87764e71b0da
wujiess <yanmingpc123@163.com>
2021-12-31 17:18:12 +0800

    完成 Money 的 HTML 和 CSS

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,19 +1,14 @@
 <template>
-  <Layout>
-    <div class="tags">
-      <ul class="current">
-        <li>衣</li>
-        <li>食</li>
-        <li>住</li>
-        <li>行</li>
-      </ul>
-      <div class="new"><button>新增标签</button></div>
-    </div>
-    <div>
-      <label class="notes">
-        <span class="name">备注</span>
-        <input type="text" placeholder="在这里输入备注"
-      /></label>
+  <Layout class-prefix="layout">
+    <div class="numberPad">
+      <div class="output">100</div>
+      <div class="buttons">
+        <button>1</button><button>2</button><button>3</button
+        ><button>删除</button><button>4</button><button>5</button
+        ><button>6</button><button>清空</button><button>7</button
+        ><button>8</button><button>9</button><button class="ok">OK</button
+        ><button class="zero">0</button><button>.</button>
+      </div>
     </div>
     <div>
       <ul class="types">
@@ -21,15 +16,20 @@
         <li>收入</li>
       </ul>
     </div>
-    <div class="numberPad">
-      <div class="output">100</div>
-      <div class="buttons">
-        <button>1</button><button>2</button><button>3</button
-        ><button>删除</button><button>4</button><button>5</button
-        ><button>6</button><button>清空</button><button>7</button
-        ><button>8</button><button>9</button><button>OK</button
-        ><button>0</button><button>.</button>
-      </div>
+    <div>
+      <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注"
+      /></label>
+    </div>
+    <div class="tags">
+      <div class="new"><button>新增标签</button></div>
+      <ul class="current">
+        <li>衣</li>
+        <li>食</li>
+        <li>住</li>
+        <li>行</li>
+      </ul>
     </div>
   </Layout>
 </template>
@@ -39,21 +39,34 @@ export default {
   name: "Money",
 };
 </script>
+<style lang="scss">
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
 <style lang="scss" scoped>
 @import "~@/assets/style/helper.scss";
 .tags {
   font-size: 14px;
   padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
   > .current {
     display: flex;
+    flex-wrap: wrap;
+    height: 13vh;
+    overflow: scroll;
     > li {
       background-color: #d9d9d9;
       $h: 24px;
       height: $h;
       line-height: $h;
       border-radius: $h/2;
-      padding: 0 12px;
+      padding: 0 16px;
       margin-right: 12px;
+      margin-top: 4px;
     }
   }
 
@@ -108,6 +121,63 @@ export default {
       width: 100%;
       height: 4px;
       background-color: #333;
+    }
+  }
+}
+
+.numberPad {
+  > .output {
+    @extend %clear-fix;
+    @extend %inner-shadow;
+    font-size: 36px;
+    font-family: Consolas, monospace;
+    padding: 9px 16px;
+    text-align: right;
+  }
+  > .buttons {
+    @extend %clear-fix;
+    > button {
+      width: 25%;
+      height: 64px;
+      float: left;
+      background: transparent;
+      border: none;
+      &.ok {
+        height: 64 * 2px;
+        float: right;
+      }
+      &.zero {
+        width: 2 * 25%;
+      }
+      $bg: #f2f2f2;
+      &:nth-child(1) {
+        background-color: $bg;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background-color: darken($bg, 4%);
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background-color: darken($bg, 2 * 4%);
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background-color: darken($bg, 3 * 4%);
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background-color: darken($bg, 4 * 4%);
+      }
+      &:nth-child(14) {
+        background-color: darken($bg, 5 * 4%);
+      }
+      &:nth-child(12) {
+        background-color: darken($bg, 6 * 4%);
+      }
     }
   }
 }
