45392e58f9623d29d3340d257684f4394da8dcd9
wujiess <yanmingpc123@163.com>
2022-01-04 15:08:35 +0800

    可以选中或取消 tag

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,11 +3,11 @@
     <NumberPad />
     <Types :xxx="123" />
     <Notes />
-    <Tags />
+    <Tags :dataSource="tags" />
   </Layout>
 </template>
 
-<script lang="ts">
+<script lang="js">
 import NumberPad from "@/components/Money/NumberPad.vue";
 import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
@@ -15,6 +15,11 @@ import Tags from "@/components/Money/Tags.vue";
 export default {
   components: { NumberPad, Types, Notes, Tags },
   name: "Money",
+  data() {
+    return  { 
+      tags: ["衣", "食", "住", "行"]
+    } 
+  }
 };
 </script>
 <style lang="scss">
