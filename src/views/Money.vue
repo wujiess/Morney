e43408a764d1b99d82ca5d4beaf9d1e3fd22849c
yanmingpc123@163.com <yanmingpc123@163.com>
2022-01-04 23:35:37 +0800

    封装出一个 model

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,5 @@
 <template>
   <Layout class-prefix="layout">
-    {{ records }}
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
     <Notes @update:value="onUpdateNotes" />
@@ -15,20 +14,17 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
+import model from "@/model";
 
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-  creationDate?: Date;
-};
+const records = model.fetch();
 
 @Component({ components: { NumberPad, Types, Notes, Tags } })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
-  records: Record[] = JSON.parse(localStorage.getItem("records") || "[]");
-  record: Record = { tags: [], notes: "", type: "-", amount: 0 };
+  // eslint-disable-next-line no-undef
+  records: RecordItem[] = records;
+  // eslint-disable-next-line no-undef
+  record: RecordItem = { tags: [], notes: "", type: "-", amount: 0 };
 
   onUpdateTags(value: string[]) {
     this.record.tags = value;
@@ -39,14 +35,15 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const r: Record = JSON.parse(JSON.stringify(this.record));
+    // eslint-disable-next-line no-undef
+    const r: RecordItem = model.clone(this.record);
     r.creationDate = new Date();
     this.records.push(r);
   }
 
   @Watch("records")
   onRecordsChanged() {
-    localStorage.setItem("records", JSON.stringify(this.records));
+    model.save(this.records);
   }
 
   onUpdateAmount(value: string) {
