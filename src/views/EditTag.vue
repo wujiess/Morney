36589bed26fff85bf1743455496eaeb23a67c039
wujiess <yanmingpc123@163.com>
2022-01-05 15:25:04 +0800

    改造 Notes.vue 使其成为通用组件

--- src/views/EditTag.vue
+++ src/views/EditTag.vue
@@ -1,13 +1,20 @@
 <template>
-  <layout>编辑标签</layout>
+  <layout>
+    <div>
+      <Icon name="left" />
+      <span>编辑标签</span>
+    </div>
+    <Notes fieldName="标签名" placeholder="请输入标签名" />
+  </layout>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
 import tagsModel from "@/models/tagsModel";
+import Notes from "@/components/Money/Notes.vue";
 
-@Component
+@Component({ components: { Notes } })
 export default class EditTag extends Vue {
   created() {
     const id = this.$route.params.id;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,7 @@
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord" />
     <Types :value.sync="record.type" />
-    <Notes @update:value="onUpdateNotes" />
+    <Notes @update:value="onUpdateNotes" fieldName="备注" placeholder="在这里输入备注" />
     <Tags :dataSource.sync="tags" @update:value="onUpdateTags" />
   </Layout>
 </template>
