0dcdcceb9079df5f1d9801517563f23ba97a061d
yanmingpc123@163.com <yanmingpc123@163.com>
2022-01-10 23:03:19 +0800

    更新 CSS

--- src/components/Money/Tabs.vue
+++ src/components/Money/Tabs.vue
@@ -1,6 +1,7 @@
 <template>
-  <ul class="tabs">
+  <ul class="tabs" :class="{ [classPrefix + '-tabs']: classPrefix }">
     <li
+      class="tabs-item"
       v-for="item in dataSource"
       :key="item.value"
       @click="select(item)"
@@ -48,7 +49,7 @@ export default class Tabs extends Vue {
   display: flex;
   text-align: center;
   font-size: 24px;
-  > li {
+  &-item {
     width: 50%;
     height: 64px;
     display: flex;
